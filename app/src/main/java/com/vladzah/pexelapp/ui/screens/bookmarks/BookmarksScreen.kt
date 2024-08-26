f0f4e4106da79830ba22f2d0c9c14159b93f32aa
Vladzaharinsky <zahar.vlad23@gmail.com>
2024-08-26 13:29:27 +0200

    Created instances of screens

---
+++ app/src/main/java/com/vladzah/pexelapp/ui/screens/bookmarks/BookmarksScreen.kt
@@ -0,0 +1,8 @@
+package com.vladzah.pexelapp.ui.screens.bookmarks
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BookmarksScreen() {
+
+}
---
+++ app/src/main/java/com/vladzah/pexelapp/ui/screens/details/DetailsScreen.kt
@@ -0,0 +1,8 @@
+package com.vladzah.pexelapp.ui.screens.details
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen() {
+
+}
