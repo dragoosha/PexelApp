c33f5a154f40e6ed5cb149145104318600fde63b
Vladzaharinsky <zahar.vlad23@gmail.com>
2024-09-01 20:30:41 +0200

    Added loading on splash screen

--- app/src/main/java/com/vladzah/pexelapp/MainActivity.kt
+++ app/src/main/java/com/vladzah/pexelapp/MainActivity.kt
@@ -3,51 +3,32 @@ package com.vladzah.pexelapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.vladzah.pexelapp.ui.navigation.AppNavigation
-import com.vladzah.pexelapp.ui.screens.home.HomeScreen
+import com.vladzah.pexelapp.ui.screens.main.MainScreen
 import com.vladzah.pexelapp.ui.theme.PexelAppTheme
+import com.vladzah.pexelapp.viewmodels.HomeScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainScreenViewModel: HomeScreenViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createSplashScreen()
+
         setContent {
             PexelAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    installSplashScreen()
-                    AppNavigation()
-                }
+                MainScreen()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PexelAppTheme {
-        Greeting("Android")
+    private fun createSplashScreen() {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { mainScreenViewModel.isLoading.value }
+        }
     }
 }
